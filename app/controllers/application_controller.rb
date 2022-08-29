f2931d3733407b4648b0cfed39ab22bed066c7d7
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-08-29 19:16:05 -0300

    updating application controller with devise parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[first_name
+                                               last_name
+                                               age
+                                               gender
+                                               document
+                                               country])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[first_name
+                                               last_name
+                                               age
+                                               gender
+                                               document
+                                               country])
+  end
 end
