7ed1ed6f37d13f656f7219e10e1bbdb39f09b8e8
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-29 18:40:50 -0300

    added name addres country website columns to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_211617) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_213830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_211617) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address"
+    t.string "country"
+    t.string "website"
     t.index ["email"], name: "index_organizations_on_email", unique: true
     t.index ["reset_password_token"], name: "index_organizations_on_reset_password_token", unique: true
   end
