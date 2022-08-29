0e10919e70e371ebe0892936a3dbf9ef4fb086ab
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-29 18:44:57 -0300

    merged

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_213342) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_213830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
