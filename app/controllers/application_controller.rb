c69f3f68093703f9889839e9d87a4d89b9d26b55
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-29 23:20:57 -0300

    Generated Organizations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_them
+
+  protected
+
+  def authenticate_them
+    if users_user_signed_in?
+      authenticate_user!
+    elsif organizations_organization_signed_in?
+      authenticate_organization!
+    end
+  end
+
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # def devise_parameter_sanitizer
