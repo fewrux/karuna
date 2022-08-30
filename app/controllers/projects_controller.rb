e1bfb01b7824854f5a7de12df3620093d859d961
Joana Areas <jo_areas@hotmail.com>
2022-08-30 16:58:13 -0300

    created first version of index - without filters

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
 
+  def index
+    @projects = policy_scope(Project)
+  end
+
   def show
     authorize @project
-  end 
+  end
 end
--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,12 +1,12 @@
 class ProjectPolicy < ApplicationPolicy
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+
+    def resolve
+      scope.all
+    end
 
     def show?
       return true
-    end 
+    end
   end
 end
