e3f4ba75296897b7d0d7b4ce352f7a017d2d4ff8
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-29 22:56:48 -0300

    test

--- app/sanitizers/organizations/parameter_sanitizer.rb
+++
@@ -1,7 +0,0 @@
-# class Organization::ParameterSanitizer < Devise::ParameterSanitizer
-#   def initialize(*)
-#     super
-#     permit(:sign_up, keys: %i[name address country website])
-#     permit(:account_update, keys: %i[name address country website])
-#   end
-# end
