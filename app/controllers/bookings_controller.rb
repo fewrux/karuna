e8cbf61f30ff49179e3f4942049226491905592a
Joana Areas <jo_areas@hotmail.com>
2022-09-01 18:27:02 -0300

    added link in project cards to project show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,7 @@ class BookingsController < ApplicationController
   def create
     @project = Project.find(params[:project_id])
     @booking = Booking.new(booking_params)
+    raise
     @booking.user_id = current_user
     @booking.project_id = @project
 
