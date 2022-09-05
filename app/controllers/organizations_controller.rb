0ef1414061ce55e533a474a2221271fe59587fe8
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-09-05 12:11:29 -0300

    Created Route, Action and View for Organization show page.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,7 @@
 class OrganizationsController < ApplicationController
+  def show
+    @organization = current_organization if current_organization.present?
+    @organization = Organization.find_by(id: params[:id]) if current_organization.nil?
+    authorize @organization
+  end
 end
