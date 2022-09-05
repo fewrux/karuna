1a8e7bb281fffb32c1b9a7c329ad807c74c81d34
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-09-05 17:32:51 -0300

    Setting organization_tabs stimulus controller.

---
+++ app/javascript/controllers/organization_tabs_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="organization-tabs"
+export default class extends Controller {
+  static targets = ["projects", "channels", "requests"]
+
+  connect() {
+    console.log("hello from tabs controller");
+  }
+
+  renderProjects() {
+    this.projectsTarget.classList.add("active");
+    this.channelsTarget.classList.remove("active");
+    this.requestsTarget.classList.remove("active");
+  }
+
+  renderChannels() {
+    this.projectsTarget.classList.remove("active");
+    this.channelsTarget.classList.add("active");
+    this.requestsTarget.classList.remove("active");
+  }
+
+  renderRequests() {
+    this.projectsTarget.classList.remove("active");
+    this.channelsTarget.classList.remove("active");
+    this.requestsTarget.classList.add("active");
+  }
+}
