688d1f85506ea7b2198cd90e32b99e1912852c34
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-09-06 20:12:45 -0300

    created project message system

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,6 +14,7 @@ class MessagesController < ApplicationController
         render_to_string(partial: "message", locals: {message: @message})
       )
       head :ok
+      authorize @message
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_171810) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_224721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_171810) do
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.bigint "chatroom_id", null: false
-    t.bigint "user_id", null: false
-    t.bigint "organization_id", null: false
+    t.bigint "user_id"
+    t.bigint "organization_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
