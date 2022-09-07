fa9f6e6a5a7db45aff9b186406c4a418ec3ec6f4
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-09-07 15:44:38 -0300

    still fixing the chatroom

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,12 @@ class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user, optional: true
   belongs_to :organization, optional: true
+
+  def sender?(a_user)
+    if user.present?
+      user.id == a_user.id
+    elsif organization.present?
+      organization.id == a_user.id
+    end
+  end
 end
